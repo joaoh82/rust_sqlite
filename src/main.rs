339cff3bef8a65827fcc7c77d1dec33029df1614
Joao Henrique Machado Silva <joaoh82@gmail.com>
2021-02-14 22:18:31 +0100

    Implemented a simple REPL with rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+mod repl;
+
+use repl::{REPLHelper, get_config};
+
+use rustyline::error::ReadlineError;
+use rustyline::{Editor};
+
+fn main() -> rustyline::Result<()> {
+    env_logger::init();
+
+    // Starting Rustyline with a default configuration
+    let config = get_config();
+
+    // Getting a new Rustyline Helper
+    let helper = REPLHelper::new();
+    
+    // Initiatlizing Rustyline Editor with set config and setting helper
+    let mut repl = Editor::with_config(config);
+    repl.set_helper(Some(helper));
+
+    // This method loads history file into memory
+    // If it doesn't exist, creates one
+    // TODO: Check history file size and if too big, clean it.
+    if repl.load_history("history").is_err() {
+        println!("No previous history.");
+    }
+    // Counter is set to improve user experience and show user how many 
+    // commands he has ran.
+    let mut count = 1;
+    loop {
+        let p = format!("rust-sqlite | {}> ", count);
+        repl.helper_mut()
+            .expect("No helper found")
+            .colored_prompt = format!("\x1b[1;32m{}\x1b[0m", p);
+        // Source for ANSI Color information: http://www.perpetualpc.net/6429_colors.html#color_list
+        // http://bixense.com/clicolors/
+
+        let readline = repl.readline(&p);
+        match readline {
+            Ok(line) => {
+                repl.add_history_entry(line.as_str());
+                // println!("Line: {}", line);
+                if line.eq(".exit") {
+                    break;
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+        count += 1;
+    }
+    repl.append_history("history").unwrap();
+
+    Ok(())
 }
---
+++ src/repl/mod.rs
@@ -0,0 +1,124 @@
+use std::borrow::Cow::{self, Borrowed, Owned};
+
+use rustyline_derive::{Helper};
+use rustyline::error::ReadlineError;
+use rustyline::config::OutputStreamType;
+use rustyline::{CompletionType, Config, Context, EditMode};
+use rustyline::validate::{MatchingBracketValidator, Validator};
+use rustyline::validate::{ValidationContext, ValidationResult};
+use rustyline::completion::{Completer, FilenameCompleter, Pair};
+use rustyline::hint::{Hinter, HistoryHinter};
+use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
+
+// REPL Helper Struct with all functionalities
+#[derive(Helper)]
+pub struct REPLHelper {
+    pub validator: MatchingBracketValidator,
+    pub colored_prompt: String,
+    pub completer: FilenameCompleter,
+    pub hinter: HistoryHinter,
+    pub highlighter: MatchingBracketHighlighter,
+}
+
+impl REPLHelper {
+    // Default constructor
+    pub fn new() -> Self {
+        REPLHelper {
+            completer: FilenameCompleter::new(),
+            highlighter: MatchingBracketHighlighter::new(),
+            hinter: HistoryHinter {},
+            colored_prompt: "".to_owned(),
+            validator: MatchingBracketValidator::new(),
+        }
+    }
+}
+
+// Implementing trait called for tab-completion
+impl Completer for REPLHelper {
+    type Candidate = Pair;
+
+    // Takes the currently edited line with the cursor position and 
+    // returns the start position and the completion candidates for the partial word to be completed.
+    fn complete(&self, line: &str, pos: usize, ctx: &Context<'_>,) -> Result<(usize, Vec<Pair>), ReadlineError> {
+        self.completer.complete(line, pos, ctx)
+    }
+}
+
+// Implementing trait responsible for providing hints
+impl Hinter for REPLHelper {
+    type Hint = String;
+
+    // Takes the currently edited line with the cursor position and returns the string that should be 
+    // displayed or None if no hint is available for the text the user currently typed
+    fn hint(&self, line: &str, pos: usize, ctx: &Context<'_>) -> Option<String> {
+        self.hinter.hint(line, pos, ctx)
+    }
+}
+
+// Implementing trait responsible for determining whether the current input buffer is valid.
+// Rustyline uses the method provided by this trait to decide whether hitting the enter key 
+// will end the current editing session and return the current line buffer to the caller of 
+// Editor::readline or variants.
+impl Validator for REPLHelper {
+
+    // Takes the currently edited input and returns a ValidationResult indicating whether it 
+    // is valid or not along with an option message to display about the result.
+    fn validate(&self, ctx: &mut ValidationContext) -> Result<ValidationResult, ReadlineError> {
+        use ValidationResult::{Incomplete, /*Invalid,*/ Valid};
+        let input = ctx.input();
+        // let result = if !input.starts_with("SELECT") {
+        //     Invalid(Some(" --< Expect: SELECT stmt".to_owned()))
+        // } else 
+        let result = if input.eq(".exit") {
+            Valid(None)
+        } else if !input.ends_with(';') {
+            Incomplete
+        } else {
+            Valid(None)
+        };
+        Ok(result)
+    }
+
+    // Configure whether validation is performed while typing or only when user presses the Enter key.
+    fn validate_while_typing(&self) -> bool {
+        self.validator.validate_while_typing()
+    }
+}
+
+// Implementing syntax highlighter with ANSI color.
+impl Highlighter for REPLHelper {
+    // Takes the prompt and returns the highlighted version (with ANSI color).
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(&'s self, prompt: &'p str, default: bool,) -> Cow<'b, str> {
+        if default {
+            Borrowed(&self.colored_prompt)
+        } else {
+            Borrowed(prompt)
+        }
+    }
+
+    // Takes the hint and returns the highlighted version (with ANSI color).
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    // Takes the currently edited line with the cursor position and returns the highlighted version (with ANSI color).
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    // Tells if line needs to be highlighted when a specific char is typed or when cursor is moved under a specific char.
+    // Used to optimize refresh when a character is inserted or the cursor is moved.
+    fn highlight_char(&self, line: &str, pos: usize) -> bool {
+        self.highlighter.highlight_char(line, pos)
+    }
+}
+
+// Returns a Config::builder with basic Editor configuration
+pub fn get_config() -> Config {
+    Config::builder()
+        .history_ignore_space(true)
+        .completion_type(CompletionType::List)
+        .edit_mode(EditMode::Emacs)
+        .output_stream(OutputStreamType::Stdout)
+        .build()
+}
