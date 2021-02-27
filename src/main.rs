d23e12aca1cb1ac330924c61a73697978feb5fcd
kangxiaoning <kxnmei@163.com>
2021-02-27 21:02:40 +0800

    fix code formatting use `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 extern crate clap;
 
+mod error;
 mod meta_command;
 mod repl;
 mod sql;
-mod error;
 
-use repl::{REPLHelper, get_config, get_command_type, CommandType};
-use meta_command::{handle_meta_command};
-use sql::{process_command};
+use meta_command::handle_meta_command;
+use repl::{get_command_type, get_config, CommandType, REPLHelper};
+use sql::process_command;
 
 use rustyline::error::ReadlineError;
-use rustyline::{Editor};
+use rustyline::Editor;
 
-use clap::{App, crate_authors, crate_description, crate_version};
+use clap::{crate_authors, crate_description, crate_version, App};
 
 fn main() -> rustyline::Result<()> {
     env_logger::init();
 
     let _matches = App::new("Rust-SQLite")
-                          .version(crate_version!())
-                          .author(crate_authors!())
-                          .about(crate_description!())
-                          .get_matches();
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .get_matches();
 
     // Starting Rustyline with a default configuration
     let config = get_config();
 
     // Getting a new Rustyline Helper
     let helper = REPLHelper::default();
-    
+
     // Initiatlizing Rustyline Editor with set config and setting helper
     let mut repl = Editor::with_config(config);
     repl.set_helper(Some(helper));
@@ -41,18 +41,19 @@ fn main() -> rustyline::Result<()> {
     }
 
     // Friendly intro message for the user
-    println!("Rust-SQLite - {}\n{}{}{}{}",
-    crate_version!(),
-    "Enter .exit to quit.\n",
-    "Enter .help for usage hints.\n",
-    "Connected to a transient in-memory database.\n",
-    "Use '.open FILENAME' to reopen on a persistent database.");
+    println!(
+        "Rust-SQLite - {}\n{}{}{}{}",
+        crate_version!(),
+        "Enter .exit to quit.\n",
+        "Enter .help for usage hints.\n",
+        "Connected to a transient in-memory database.\n",
+        "Use '.open FILENAME' to reopen on a persistent database."
+    );
 
     loop {
         let p = format!("sqlrite> ");
-        repl.helper_mut()
-            .expect("No helper found")
-            .colored_prompt = format!("\x1b[1;32m{}\x1b[0m", p);
+        repl.helper_mut().expect("No helper found").colored_prompt =
+            format!("\x1b[1;32m{}\x1b[0m", p);
         // Source for ANSI Color information: http://www.perpetualpc.net/6429_colors.html#color_list
         // http://bixense.com/clicolors/
 
@@ -66,7 +67,7 @@ fn main() -> rustyline::Result<()> {
                         // process_command takes care of tokenizing, parsing and executing
                         // the SQL Statement and returning a Result<String, SQLRiteError>
                         let _ = match process_command(&command) {
-                            Ok(response) => println!("{}",response),
+                            Ok(response) => println!("{}", response),
                             Err(err) => println!("An error occured: {}", err),
                         };
                     }
@@ -74,7 +75,7 @@ fn main() -> rustyline::Result<()> {
                         // handle_meta_command parses and executes the MetaCommand
                         // and returns a Result<String, SQLRiteError>
                         let _ = match handle_meta_command(cmd) {
-                            Ok(response) => println!("{}",response),
+                            Ok(response) => println!("{}", response),
                             Err(err) => println!("An error occured: {}", err),
                         };
                     }
